0465d381c16b4febbcf52a736ac4aa34469e64a2
aryan1403 <aaryan14032006@gmail.com>
2025-10-29 09:12:52 +0530

    blankd project

--- tests/reactest.spec.js
+++
@@ -1,8 +0,0 @@
-const { expect, test } = require("playwright/test");
-
-test("check the learn react link", async ({ page }) => {
-  await page.goto('/')
-  await page.locator('#root > div > header > a').click()
-  console.log(page.url.toString())
-  await expect(page).toHaveURL('http://localhost:3000')
-});
